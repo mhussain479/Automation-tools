bc8347339aabd279e3e1414087602cf99ed0d161
mhussain479 <mhussain479@outlook.com>
2026-06-10 16:07:36 +0500

    Update inbox_cleaner.py

--- inbox_cleaner.py
+++ inbox_cleaner.py
@@ -6,7 +6,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
+
 SCOPES = ["https://mail.google.com/"]
 
 
@@ -15,12 +15,10 @@ def main():
   Lists the user's Gmail labels.
   """
   creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
+
   if os.path.exists("token.json"):
     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
+    
   if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
       creds.refresh(Request())
@@ -29,7 +27,7 @@ def main():
           "credentials.json", SCOPES
       )
       creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
+      
     with open("token.json", "w") as token:
       token.write(creds.to_json())
 
@@ -72,7 +70,6 @@ def main():
         print("Cancelled.")
 
   except HttpError as error:
-    # TODO(developer) - Handle errors from gmail API.
     print(f"An error occurred: {error}")
 
 
